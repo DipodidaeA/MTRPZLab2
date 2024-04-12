34a6d5f6c61f67f356f3527e86e61cbedd053bf7
Anton Rak <anton.rak050@gmail.com>
2024-04-12 13:42:54 +0300

    Func Chenge. Func Test
    Створено функцію перетворення тексту та тести для функції.

---
+++ Lab2/ChengeMarkText.go
@@ -0,0 +1,175 @@
+package lab2
+
+import (
+	"strings"
+	"unicode/utf8"
+)
+
+func ChengeText(textInput string) (string, string) {
+	textNew := ""
+	var textSize int
+	preOpen := 0
+	ttOpen := 0
+	bOpen := 0
+	iOpen := 0
+	err := ""
+	textInput = strings.ReplaceAll(textInput, "\n", " ")
+	textSize = utf8.RuneCountInString(textInput)
+	textInput += "FORONEERROR"
+	textNew += "<p>"
+	for i := 0; i < textSize; i++ {
+		if err == "" {
+			switch textInput[i] {
+			case '`':
+				if textInput[i+1] == '`' {
+					if textInput[i+2] == '`' {
+						i += 3
+						preOpen = 1
+						textNew += "<pre>"
+						for i < textSize {
+							if preOpen == 0 {
+								break
+							} else {
+								switch textInput[i] {
+								case '`':
+									if textInput[i+1] == '`' && textInput[i+2] == '`' && textInput[i+3] != '`' {
+										preOpen = 0
+										i += 2
+										textNew += "</pre>"
+									} else {
+										textNew += string(textInput[i])
+										i++
+									}
+								default:
+									textNew += string(textInput[i])
+									i++
+								}
+							}
+						}
+					} else {
+						textNew += string(textInput[i])
+					}
+				} else {
+					if textInput[i+1] != ' ' && textInput[i+1] != '*' && textInput[i+1] != '_' {
+						i++
+						ttOpen = 1
+						textNew += "<tt>"
+						for i < textSize {
+							if ttOpen == 0 {
+								break
+							} else {
+								switch textInput[i] {
+								case '`':
+									if textInput[i-1] != ' ' {
+										ttOpen = 0
+										textNew += "</tt>"
+									} else {
+										ttOpen = 0
+										err = "Error char `"
+									}
+								default:
+									textNew += string(textInput[i])
+									i++
+								}
+							}
+						}
+					} else {
+						textNew += string(textInput[i])
+					}
+				}
+			case '*':
+				if textInput[i+1] == '*' {
+					if textInput[i+2] == '*' {
+						textNew += string(textInput[i])
+					} else {
+						if textInput[i+2] != ' ' && textInput[i+2] != '`' && textInput[i+2] != '_' {
+							i += 2
+							bOpen = 1
+							textNew += "<b>"
+							for i < textSize {
+								if bOpen == 0 {
+									break
+								} else {
+									switch textInput[i] {
+									case '*':
+										if textInput[i-1] != ' ' && textInput[i+1] == '*' {
+											bOpen = 0
+											textNew += "</b>"
+											i++
+										} else {
+											bOpen = 0
+											err = "Error char *"
+										}
+									default:
+										textNew += string(textInput[i])
+										i++
+									}
+								}
+							}
+						} else {
+							textNew += string(textInput[i])
+						}
+					}
+				} else {
+					textNew += string(textInput[i])
+				}
+			case '_':
+				if textInput[i+1] == '_' {
+					if textInput[i+2] == '_' {
+						textNew += string(textInput[i])
+					} else {
+						textNew += string(textInput[i])
+					}
+				} else {
+					if textInput[i+1] != ' ' && textInput[i+1] != '*' && textInput[i+1] != '`' {
+						i++
+						iOpen = 1
+						textNew += "<i>"
+						for i < textSize {
+							if iOpen == 0 {
+								break
+							} else {
+								switch textInput[i] {
+								case '_':
+									if textInput[i-1] != ' ' {
+										iOpen = 0
+										textNew += "</i>"
+									} else {
+										iOpen = 0
+										err = "Error char _"
+									}
+								default:
+									textNew += string(textInput[i])
+									i++
+								}
+							}
+						}
+					} else {
+						textNew += string(textInput[i])
+					}
+				}
+			case ' ':
+				if textInput[i+1] == ' ' {
+					if textInput[i+2] == ' ' {
+						break
+					} else {
+						textNew += "</p><p>"
+						i++
+					}
+				} else {
+					textNew += string(textInput[i])
+				}
+			default:
+				textNew += string(textInput[i])
+			}
+		} else {
+			break
+		}
+	}
+
+	if err == "" && preOpen == 0 && ttOpen == 0 && iOpen == 0 && bOpen == 0 {
+		textNew += "</p>"
+		textNew = strings.ReplaceAll(textNew, "FORONEERROR", "")
+	}
+	return textNew, err
+}
---
+++ Lab2/ChengeMarkText_test.go
@@ -0,0 +1,32 @@
+package lab2
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestChengeText(t *testing.T) {
+	for _, tc := range []struct {
+		nameTest string
+		textIn   string
+		resText  string
+	}{
+		{nameTest: "1", textIn: "Test", resText: "<p>Test</p>"},
+		{nameTest: "2", textIn: "**Test**", resText: "<p><b>Test</b></p>"},
+		{nameTest: "3", textIn: "_Test_", resText: "<p><i>Test</i></p>"},
+		{nameTest: "4", textIn: "`Test`", resText: "<p><tt>Test</tt></p>"},
+		{nameTest: "5", textIn: "```Test```", resText: "<p><pre>Test</pre></p>"},
+		{nameTest: "6", textIn: "T  e  s  t", resText: "<p>T</p><p>e</p><p>s</p><p>t</p>"},
+		{nameTest: "7", textIn: "```T*e`s_t```", resText: "<p><pre>T*e`s_t</pre></p>"},
+		{nameTest: "8", textIn: "_ Test", resText: "<p>_ Test</p>"},
+		{nameTest: "9", textIn: "Te_st", resText: "<p>Te_st</p>"},
+	} {
+		t.Run(tc.nameTest, func(t *testing.T) {
+			if got, textErr := ChengeText(tc.textIn); textErr != "" {
+				t.Errorf("Have error: %s. ResText: %s, got: %s", textErr, tc.resText, got)
+			} else {
+				fmt.Println("ResText:", tc.resText, "got:", got)
+			}
+		})
+	}
+}
